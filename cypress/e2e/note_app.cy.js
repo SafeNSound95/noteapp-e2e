8b0f484e05f9337811f3af79d5f3096d607fba9b
Saif Shalan <safensound95@gmail.com>
2025-08-13 22:11:31 +0300

    Start Cypress E2E testing

---
+++ cypress/e2e/note_app.cy.js
@@ -0,0 +1,40 @@
+describe('Note app', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:5173')
+  })
+
+  it('front page can be opened', function() {
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2025')
+  })
+
+  it('login form can be opened', function() {
+    cy.contains('login').click()
+  })
+
+  it('user can login', function () {
+    cy.contains('login').click()
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('salainen')
+    cy.get('#login-button').click()
+    cy.contains('Matti Luukkainen logged-in')
+  }) 
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('login').click()
+      cy.get('input:first').type('mluukkai')
+      cy.get('input:last').type('salainen')
+      cy.get('#login-button').click()
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+  })
+
+})
+
