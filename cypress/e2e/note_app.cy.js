45d5c5d6f389583cd1c5dfee75c60bb30d7ffeeb
Saif Shalan <safensound95@gmail.com>
2025-08-17 05:49:08 +0300

    add more tests

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,6 +1,13 @@
 describe('Note app', function() {
   beforeEach(function() {
     cy.visit('http://localhost:5173')
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user) 
   })
 
   it('front page can be opened', function() {
@@ -10,6 +17,19 @@ describe('Note app', function() {
 
   it('login form can be opened', function() {
     cy.contains('login').click()
+  })
+
+  
+  it('login fails with wrong password', function() {
+    cy.contains('login').click()
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
+
+    cy.get('.error')
+    .should('contain', 'Wrong credentials')
+    .and('have.css', 'color', 'rgb(255, 0, 0)')
+    .and('have.css', 'border-style', 'solid')
   })
 
   it('user can login', function () {
@@ -33,6 +53,23 @@ describe('Note app', function() {
       cy.get('input').type('a note created by cypress')
       cy.contains('save').click()
       cy.contains('a note created by cypress')
+    })
+
+    describe('and a note exists', function() {
+     beforeEach(function () {
+        cy.contains('new note').click()
+        cy.get('input').type('another note cypress')
+        cy.contains('save').click()
+      })
+
+      it('it can be made not important', function () {
+        cy.contains('another note cypress')
+          .contains('make not important')
+          .click()
+
+        cy.contains('another note cypress')
+          .contains('make important')
+      })
     })
   })
 
